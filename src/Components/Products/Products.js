ee4239cc14db7a52509dd3e60081bd6fc3fb98fc
anikshajol <anikshajol@gmail.com>
2022-09-29 19:01:30 +0600

    fake data json update and Product card update

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -2,18 +2,23 @@ import React from "react";
 import "./Products.css";
 
 const Products = ({ product }) => {
+  const { age, name, picture, time, toAge } = product;
   console.log(product);
+
   return (
     <section className="product-card-container grid grid-cols-3 gap-32 mt-20 ">
       <div className="card card-compact w-72 bg-base-100 shadow-xl">
         <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+          <img src={picture} alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <h2 className="card-title">{name}</h2>
+          <p>
+            For Age: {age}-{toAge}
+          </p>
+          <p>Time required: {time}s</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary">Add</button>
           </div>
         </div>
       </div>
