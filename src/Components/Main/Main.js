81f03b90c5592b2e8815c1bac1dd2cb01a111d6d
anikshajol <anikshajol@gmail.com>
2022-09-29 13:02:55 +0600

    style added

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Main.css";
 import Products from "../Products/Products";
 
 const Main = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
-    <div>
-      <section className="card-container mt-20 container mx-auto">
-        <Products></Products>
+    <div className="main-container container mx-auto">
+      <section className="card-container  grid grid-cols-3 gap-30 ">
+        {products.map((product) => (
+          <Products key={product._id} product={product}></Products>
+        ))}
       </section>
 
-      <section className="side-bar"></section>
+      <section className="side-bar flex">
+        <h2>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          facere ut? Sit porro voluptates quos perferendis nisi suscipit dolor
+          numquam, aliquam provident, eaque in sequi ea similique maxime
+          dolorem. Commodi.
+        </h2>
+      </section>
     </div>
   );
 };
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,10 +1,23 @@
 import React from "react";
+import "./Products.css";
 
-const Products = () => {
+const Products = ({ product }) => {
+  console.log(product);
   return (
-    <div>
-      <h className="text-3xl">Hi i am from Main for card </h>
-    </div>
+    <section className="product-card-container grid grid-cols-3 gap-32 mt-20 ">
+      <div className="card card-compact w-72 bg-base-100 shadow-xl">
+        <figure>
+          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
