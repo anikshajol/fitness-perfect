d72def59f2e02d49e2e46bb1462e6679c419df78
anikshajol <anikshajol@gmail.com>
2022-09-30 15:57:56 +0600

    break time fixed

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -5,6 +5,8 @@ import SideBar from "../Sidebar/SideBar";
 
 const Main = () => {
   const [products, setProducts] = useState([]);
+  const [second, setSecond] = useState([]);
+  const [time, setTime] = useState("00s");
 
   useEffect(() => {
     fetch("data.json")
@@ -12,16 +14,125 @@ const Main = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  const handleAdd = (products) => {
+    console.log(products);
+    const newSecond = [...second, products];
+    setSecond(parseFloat(newSecond));
+    console.log(newSecond);
+  };
+
+  const handleBreakTime = (e) => {
+    const newTime = e.target.innerText;
+    setTime(newTime);
+    console.log(newTime);
+  };
+
   return (
     <div className="main-container container mx-auto">
       <section className="card-container  grid grid-cols-3  ">
         {products.map((product) => (
-          <Products key={product._id} product={product}></Products>
+          <Products
+            key={product._id}
+            product={product}
+            handleAdd={handleAdd}
+          ></Products>
         ))}
       </section>
 
       <section className="side-bar h-full bg-white-500">
         <SideBar></SideBar>
+        <section className="ml-1">
+          <div className="user-profile">
+            <div className="display-image">
+              <img src={require("../../display-image.png")} alt="" />
+            </div>
+            <div className="user-name">
+              <p>Md Anik Hasan</p>
+              <small className="text-gray-400">Dhaka, bangladesh</small>
+            </div>
+          </div>
+        </section>
+
+        <section className="user-info grid grid-cols-3 gap-4">
+          <div className="age">
+            <h2>
+              29<span className="text-xs text-gray-500">yrs</span>
+            </h2>
+            <p>Age</p>
+          </div>
+
+          <div className="weight">
+            <h2>
+              75<span className="text-xs text-gray-500">kg</span>
+            </h2>
+            <p>Weight</p>
+          </div>
+          <div className="height">
+            <h2>5'6</h2>
+            <p>Height</p>
+          </div>
+        </section>
+
+        <section className="mt-12 ml-4 text-3xl ">
+          <h2>Add a Break</h2>
+          <div>
+            <section className="user-info grid grid-cols-4 gap-4">
+              <div className="age">
+                <h2 onClick={(e) => handleBreakTime(e)} className="second">
+                  29s
+                </h2>
+              </div>
+
+              <div className="weight">
+                <h2 onClick={(e) => handleBreakTime(e)} className="second">
+                  75s
+                </h2>
+              </div>
+              <div className="height">
+                <h2 onClick={(e) => handleBreakTime(e)} className="second">
+                  30s
+                </h2>
+              </div>
+              <div className="height">
+                <h2 onClick={(e) => handleBreakTime(e)} className="second">
+                  25s
+                </h2>
+              </div>
+            </section>
+          </div>
+        </section>
+
+        <section className="mt-12 ml-4 text-3xl ">
+          <h2>Exercise Details</h2>
+          <div>
+            <section className="user-info flex justify-around gap-4">
+              <div className="exercise-time">
+                <h2>Exercise time</h2>
+              </div>
+              <div className="time">
+                <p>
+                  {second}
+                  <span>seconds</span>
+                </p>
+              </div>
+            </section>
+
+            <section className="user-info flex justify-around gap-4">
+              <div className="exercise-time">
+                <h2>Break time</h2>
+              </div>
+              <div className="time">
+                <p id="addTime">{time}</p>
+              </div>
+            </section>
+          </div>
+        </section>
+
+        <div className=" flex justify-center mt-12">
+          <button className="btn btn-active btn-primary w-2/4">
+            Completed Activity
+          </button>
+        </div>
       </section>
     </div>
   );
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "./Products.css";
 
-const Products = ({ product }) => {
+const Products = ({ product, handleAdd }) => {
   const { age, name, picture, time, toAge } = product;
-  console.log(product);
 
   // const [cart, setCart] = useState([]);
 
@@ -18,9 +17,14 @@ const Products = ({ product }) => {
           <p>
             For Age: {age}-{toAge}
           </p>
-          <p>Time required: {time}s</p>
+          <p>
+            Time required: {time}
+            <span>s</span>
+          </p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Add</button>
+            <button onClick={() => handleAdd(time)} className="btn btn-primary">
+              Add
+            </button>
           </div>
         </div>
       </div>
